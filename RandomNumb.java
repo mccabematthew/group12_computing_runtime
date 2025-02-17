c23a40f4c917ef22f6545d6156e4e5a403ecfe4c
SantiPrograms <santiagoyuriar@gmail.com>
2025-02-17 14:12:38 -0600

    Add files via upload

---
+++ RandomNumb.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RandomNumb {
+	public static void main(String[] args) {
+		int array = 1000;
+		int [] randomArray = new int[array];
+		
+		Random random = new Random();
+		
+		for (int i = 0; i < array; i++) {
+			randomArray[i] = random.nextInt(1000);
+		}
+		
+		try (FileWriter writer = new FileWriter("RandomNumb.txt")) {
+            System.out.print("Random array: [");
+            for (int i = 0; i < array; i++) {
+                writer.write(randomArray[i] + "\n");
+                System.out.print(randomArray[i]);
+                if (i < array - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println("]");
+		} catch (IOException e) {
+			
+		}
+	
+	}
+}
