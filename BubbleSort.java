d44aa08450bc95a55d83f9cef68832dccd25b2e4
DoUswet-Bro <166197674+DoUswet-Bro@users.noreply.github.com>
2025-02-17 14:43:08 -0600

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class SortNumbers {
+public class BubbleSort {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
         
