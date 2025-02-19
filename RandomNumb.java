8992db4248d15b8af521473b558d216a360be71a
PattyPrograms <patricktilotta@gmail.com>
2025-02-19 14:04:01 -0600

    changed to 999 range

--- RandomNumb.java
+++ RandomNumb.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 
 public class RandomNumb {
 	public static void main(String[] args) {
-		int array = 1000;
+		int array = 999;
 		int [] randomArray = new int[array];
 		
 		Random random = new Random();
 		
 		for (int i = 0; i < array; i++) {
-			randomArray[i] = random.nextInt(1000);
+			randomArray[i] = random.nextInt(999);
 		}
 		
 		try (FileWriter writer = new FileWriter("RandNumb.txt")) {
