eea7d281f9625580ad7d0f57b80203f5e164853e
DoUswet-Bro <166197674+DoUswet-Bro@users.noreply.github.com>
2025-02-19 15:26:18 -0600

    added Shuffle from collections
    should solve off by 1 problem

--- RandomNumb.java
+++ RandomNumb.java
@@ -1,33 +1,38 @@
-package main;
-
 import java.util.Random;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class RandomNumb {
-	public static void main(String[] args) {
-		int array = 999;
-		int [] randomArray = new int[array];
-		
-		Random random = new Random();
-		
-		for (int i = 0; i < array; i++) {
-			randomArray[i] = random.nextInt(999);
-		}
-		
-		try (FileWriter writer = new FileWriter("RandNumb.txt")) {
+    public static void main(String[] args) {
+        int arraySize = 1000;
+        int[] randomArray = new int[arraySize];
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < arraySize; i++) {
+            numbers.add(i);
+        }
+
+        Collections.shuffle(numbers, new Random());
+
+        for (int i = 0; i < arraySize; i++) {
+            randomArray[i] = numbers.get(i);
+        }
+	    
+        try (FileWriter writer = new FileWriter("RandNumb.txt")) {
             System.out.print("Random array: [");
-            for (int i = 0; i < array; i++) {
+            for (int i = 0; i < arraySize; i++) {
                 writer.write(randomArray[i] + "\n");
                 System.out.print(randomArray[i]);
-                if (i < array - 1) {
+                if (i < arraySize - 1) {
                     System.out.print(", ");
                 }
             }
             System.out.println("]");
-		} catch (IOException e) {
-			
-		}
-	
-	}
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
 }
